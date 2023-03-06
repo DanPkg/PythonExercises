56856561f5a8e6b44472a3f3778117a3baa36577
DanPkg <71873522+DanPkg@users.noreply.github.com>
2023-03-06 14:31:07 +1030

    Update conversions.py

--- topic-1/conversions.py
+++ topic-1/conversions.py
@@ -6,7 +6,7 @@ kgs = pds * KGS_PER_POUND
 print(f"{kgs} kilograms")
 print(pds, 'is equivalent to', format(kgs, '.2f'), 'kilograms')
 
-# Convert and display celsius to fahrenheit to from user input
+# Convert and display celsius to fahrenheit from user input
 temp_celsius = float(input('Enter the temperature in Celsius units: '))
 conv_to_fahren = (9/5) * temp_celsius + 32
 print(f"{conv_to_fahren:.2f} Fahrenheit")
